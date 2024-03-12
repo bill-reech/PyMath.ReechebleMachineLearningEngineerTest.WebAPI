3f9a2afbdd9b2476fa7fee5fbff3628e6fa4fd1c
MliloM <mduduzimlilo@hotmail.com>
2024-03-12 18:41:04 +0200

    chore: Implement property-based tests in finance domain
    Reconfigured the existing unit tests in the finance domain to use property-based testing. This change was made to enhance the robustness of the tests. The Hypothesis library was utilized to generate a variety of conditions to test the logic under different scenarios.

--- reecheble_finance/domain/tests/test_reducing_balance_method.py
+++ reecheble_finance/domain/tests/test_reducing_balance_method.py
@@ -1,32 +1,76 @@
 import pytest
 
+from hypothesis import given
+from hypothesis.strategies import floats, integers
+
 from reecheble_finance.domain.exceptions.domain_exceptions import InvalidLoanRequestDomainException
 from reecheble_finance.domain.models.account import Account
 from reecheble_finance.domain.models.loan_request import LoanRequest
 from reecheble_finance.domain.models.user import User
 
 
-def test_given_valid_loan_request_can_not_take_loan_with_outstanding_balance():
-    user_model = User(first_name="Mduduzi", last_name="Mlilo")
+@given(account_balance=floats(min_value=0.01, max_value=5000),
+       interest_rate=floats(min_value=0.05, max_value=0.5),
+       payment_period=integers(min_value=3, max_value=60),
+       loan_amount=floats(min_value=5000, max_value=100000))
+def test_given_valid_loan_request_can_not_take_loan_with_outstanding_balance(
+        account_balance,
+        interest_rate,
+        payment_period,
+        loan_amount):
+
+    # Arrange
+    user_model = User(first_name="John", last_name="Doe")
     account_model = Account(user=user_model)
-    account_model.outstanding_balance = 1000
+    account_model.outstanding_balance = account_balance
 
-    with pytest.raises(InvalidLoanRequestDomainException):
-        loan_request = LoanRequest(account=account_model, interest_rate=0.1, payment_period_in_months=18)
-        loan_request.request_loan(100000)
+    # Assert & Act
+    with (pytest.raises(InvalidLoanRequestDomainException)):
+        LoanRequest(
+            account=account_model,
+            interest_rate=interest_rate,
+            payment_period_in_months=payment_period
+        ).request_loan(loan_amount)
 
 
-def test_given_valid_loan_request_adds_a_new_loan_to_account():
-    user_model = User(first_name="Mduduzi", last_name="Mlilo")
+@given(interest_rate=floats(min_value=0.05, max_value=0.5),
+       payment_period=integers(min_value=3, max_value=60),
+       loan_amount=floats(min_value=5000, max_value=100000))
+def test_given_valid_loan_request_adds_a_new_loan_to_account(interest_rate, payment_period, loan_amount):
+
+    # Arrange
+    user_model = User(first_name="Zanele", last_name="Mokoena")
     account_model = Account(user=user_model)
-    loan_request = LoanRequest(account=account_model, interest_rate=0.1, payment_period_in_months=18)
-    loan_request.request_loan(request_amount=100000)
-    assert account_model.outstanding_balance == 100000
+
+    # Act
+    LoanRequest(
+        account=account_model,
+        interest_rate=interest_rate,
+        payment_period_in_months=payment_period
+    ).request_loan(request_amount=loan_amount)
+
+    # Assert
+    assert account_model.outstanding_balance == loan_amount
 
 
-def test_given_valid_loan_request_emi_property_set():
-    user_model = User(first_name="Mduduzi", last_name="Mlilo")
+@given(interest_rate=floats(min_value=0.05, max_value=0.5),
+       payment_period=integers(min_value=12, max_value=60),
+       loan_amount=floats(min_value=5000, max_value=100000))
+def test_given_valid_loan_request_emi_property_set(interest_rate, payment_period, loan_amount):
+    # Arrange
+    user_model = User(first_name="Kobus", last_name="Ndlovu")
     account_model = Account(user=user_model)
-    loan_request = LoanRequest(account=account_model, interest_rate=0.1, payment_period_in_months=18)
-    loan_request.request_loan(request_amount=100000)
-    assert loan_request.equated_monthly_instalment == LoanRequest.get_equated_monthly_installment(loan_amount=100000, monthly_interest_rate=0.1, number_of_installments=18)
+
+    # Act
+    loan_request = LoanRequest(
+        account=account_model,
+        interest_rate=interest_rate,
+        payment_period_in_months=payment_period)
+    loan_request.request_loan(request_amount=loan_amount)
+    emi = LoanRequest.get_equated_monthly_installment(
+        loan_amount=loan_amount,
+        monthly_interest_rate=interest_rate,
+        number_of_installments=payment_period)
+
+    # Assert
+    assert loan_request.equated_monthly_instalment == emi
