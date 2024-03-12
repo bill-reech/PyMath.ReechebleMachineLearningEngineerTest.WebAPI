7864c8112f23964a885ad2fed7424558ba5ed935
MliloM <mduduzimlilo@hotmail.com>
2024-03-12 20:27:42 +0200

    refactor: Move LoanRequest model to different module
    The LoanRequest model was relocated from the 'models' module to a new 'load_request' module. Updates were made in the test file to correctly import the LoanRequest from its new location. This structural change is aimed at improving the organization of domain models.

--- reecheble_finance/domain/models/__init__.py
+++ reecheble_finance/domain/models/__init__.py
@@ -4,4 +4,3 @@ Domain models. The parent module for all the domain models of the application.
 .. note:: All application's domain models should be declared withing this module.
 """
 
-from .loan_request import *
