0d42a1d284e86890c369b338e144d87b1280445d
MliloM <mduduzimlilo@hotmail.com>
2024-03-15 22:35:59 +0200

    chore: Enhance AddLoanApplicationResponseDTO
    Updated the AddLoanApplicationResponseDTO by inheriting from BaseDomainParserMixin, adding new attributes such as loan_amount and loan_granted, and marking the id attribute as optional. A similar enhancement was made to the LoanAccountDTO class. These updates facilitate a more comprehensive and flexible loan application response process.
    
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

--- reecheble_finance/application/sdk/dtos/loan_account_dto.py
+++ reecheble_finance/application/sdk/dtos/loan_account_dto.py
@@ -2,6 +2,10 @@ from pydantic import PositiveFloat
 
 from reecheble_finance.domain.abstract_domain.abstract_domain_parser_mixin import BaseDomainParserMixin
 
+__all__ = [
+    "LoanAccountDTO"
+]
+
 
 class LoanAccountDTO(BaseDomainParserMixin):
     outstanding_balance: PositiveFloat = 0.00
