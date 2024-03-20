b9e60db4e3828bd757b3b9c1c53494dca884e7d1
MliloM <mduduzi.mlilo@hotmail.com>
2024-03-20 22:13:26 +0200

    refactor: Refactor import order in add_loan_application_handler.py
    The import order in 'add_loan_application_handler.py' within the 'loan_service' service was rearranged. The changes include specifically pointing the AddLoanApplicationCommand import to its precise location, improving the readability and simplifying future code maintenance.

--- reecheble_finance/application/services/loan_service/add_loan/add_loan_application_handler.py
+++ reecheble_finance/application/services/loan_service/add_loan/add_loan_application_handler.py
@@ -4,13 +4,14 @@ A loan application command handler module.
 
 from typing import Type
 
+from reecheble_finance.domain.models.loan_request import LoanRequest
+from reecheble_finance.application.services.abstract_service import AbstractApplicationService
+from reecheble_finance.infrastructure.data_access.repositories.loan_repository.abstract_loan_repository import (
+    AbstractLoanRepository)
+from reecheble_finance.application.services.loan_service.add_loan.add_loan_application_command import (
+    AddLoanApplicationCommand)
 from reecheble_finance.application.sdk.dtos.add_loan_application.add_loan_application_response_dto import (
     AddLoanApplicationResponseDTO)
-from reecheble_finance.application.services.abstract_service import AbstractApplicationService
-from reecheble_finance.application.services.loan_service.add_loan.add_loan_application_command import AddLoanApplicationCommand
-from reecheble_finance.domain.models.loan_request import LoanRequest
-from reecheble_finance.infrastructure.data_access.repositories.loan_repository.abstract_loan_repository import \
-    AbstractLoanRepository
 
 
 class AddLoanApplicationCommandHandler(AbstractApplicationService):
