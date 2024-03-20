66f7b8e4b743cc69273c990d948783ee6b415028
MliloM <mduduzi.mlilo@hotmail.com>
2024-03-20 21:06:29 +0200

    chore: Remove unused modules and refactor database contexts
    Deleted unused import statements in multiple files and updated __init__.py scripts accordingly. Also, enhanced the structure of the connection_context and context_types modules in the database management system. This cleanup and restructuring improve code readability and efficiency.

--- reecheble_finance/infrastructure/data_access/database/databases_tools/contexts/context_types.py
+++ reecheble_finance/infrastructure/data_access/database/databases_tools/contexts/context_types.py
@@ -1,3 +1,30 @@
-class PyMongoDbContext:
-    def context(self):
-        pass
+from pymongo import MongoClient
+
+from reecheble_finance.infrastructure.data_access.context import ServiceContext
+from reecheble_finance.infrastructure.data_access.database.databases_tools.contexts.connection_context import (
+    mongo_database_context)
+
+__all__ = [
+    "PyMongoDbContext"
+]
+
+
+class PyMongoDbContext(ServiceContext):
+    session: MongoClient = None
+
+    def __init__(self) -> None:
+        self.mongo_session: MongoClient = self.__class__.session or self.get_database_context()
+        super().__init__(context=self.mongo_session)
+
+    def close(self) -> None:
+        self.context.close()
+
+    def commit(self) -> None:
+        self.context.commit()
+
+    def rollback(self) -> None:
+        self.context.rollback()
+
+    @staticmethod
+    def get_database_context():
+        return mongo_database_context()
