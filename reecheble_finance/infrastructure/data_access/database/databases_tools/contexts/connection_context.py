794b3cdd6715f69659ce1255496702117d017ebe
MliloM <mduduzimlilo@hotmail.com>
2024-03-21 09:05:01 +0200

    refactor: Update database connection handling
    Cleaned up the database connection management by centralising the MongoClient instantiation. The mongo_client creation now happens within 'connections.py' and is then imported in 'connection_context.py' as needed. The import from 'database_engines.py' has been removed as it is no longer necessary.
    
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

--- reecheble_finance/infrastructure/data_access/database/databases_tools/contexts/connection_context.py
+++ reecheble_finance/infrastructure/data_access/database/databases_tools/contexts/connection_context.py
@@ -2,10 +2,10 @@ from typing import Callable
 
 from pymongo import MongoClient
 
+from reecheble_finance.infrastructure.data_access.database.databases_tools.connections.connections import mongo_client
 
 __all__ = [
     "mongo_database_context"
 ]
 
-
-mongo_database_context: Callable[[], MongoClient] = lambda: MongoClient('localhost', 27017)
+mongo_database_context: Callable[[], MongoClient] = lambda: mongo_client
