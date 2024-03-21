0959c2976b0892033a71a4586638006ba3c0e41d
MliloM <mduduzimlilo@hotmail.com>
2024-03-21 09:46:39 +0200

    chore: Revise type hinting and object instantiation in finance service
    Improved type hinting and object instantiation in the "AbstractApplicationService" and "AddLoanApplicationCommandHandler" classes. The application now uses more specific class objects, like using "RepositoryContextSession" for context in the abstract service, and directly initializing "LoanRepository" in the loan service. The changes aim to enhance code readability and maintain consistency across the application.
    
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

--- reecheble_finance/application/services/loan_service/add_loan/add_loan_application_handler.py
+++ reecheble_finance/application/services/loan_service/add_loan/add_loan_application_handler.py
@@ -1,24 +1,23 @@
 """
 A loan application command handler module.
 """
-
-from typing import Type
-
-from reecheble_finance.domain.models.loan_request import LoanRequest
-from reecheble_finance.application.services.abstract_service import AbstractApplicationService
-from reecheble_finance.infrastructure.data_access.repositories.loan_repository.abstract_loan_repository import (
-    AbstractLoanRepository)
-from reecheble_finance.application.services.loan_service.add_loan.add_loan_application_command import (
-    AddLoanApplicationCommand)
 from reecheble_finance.application.sdk.dtos.add_loan_application.add_loan_application_response_dto import (
     AddLoanApplicationResponseDTO)
+from reecheble_finance.application.services.abstract_service import AbstractApplicationService
+from reecheble_finance.application.services.loan_service.add_loan.add_loan_application_command import (
+    AddLoanApplicationCommand)
+from reecheble_finance.domain.abstract_repository.abstract_repository import RepositoryContextSession
+from reecheble_finance.domain.models.loan_request import LoanRequest
+from reecheble_finance.infrastructure.data_access.repositories.loan_repository.abstract_loan_repository import (
+    AbstractLoanRepository)
+from reecheble_finance.infrastructure.data_access.repositories.loan_repository.loan_repository import LoanRepository
 
 
 class AddLoanApplicationCommandHandler(AbstractApplicationService):
 
-    def __init__(self, context, repository: Type[AbstractLoanRepository]) -> None:
+    def __init__(self, context: RepositoryContextSession) -> None:
         super().__init__(context=context)
-        self.repository: AbstractLoanRepository = repository(context=self.context)
+        self.repository: AbstractLoanRepository = LoanRepository(context=self.context)
 
     async def handle(self, command: AddLoanApplicationCommand) -> AddLoanApplicationResponseDTO:
         """
