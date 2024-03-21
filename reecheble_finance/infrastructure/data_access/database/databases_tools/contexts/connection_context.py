28d10f2b075677ad2a280d953b357ac7dc850c69
MliloM <mduduzimlilo@hotmail.com>
2024-03-21 09:09:47 +0200

    chore: Deleted database connection files and imports
    The files 'connections.py' and its import statement from '__init__.py' have been deleted. The database connection instance, mongo_client, is now directly created within 'connection_context.py'. Additionally, unneeded import statements in '__init__.py' and 'connection_context.py' have been removed to tidy up the code.
    
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

--- reecheble_finance/infrastructure/data_access/database/databases_tools/contexts/connection_context.py
+++ reecheble_finance/infrastructure/data_access/database/databases_tools/contexts/connection_context.py
@@ -2,10 +2,8 @@ from typing import Callable
 
 from pymongo import MongoClient
 
-from reecheble_finance.infrastructure.data_access.database.databases_tools.connections.connections import mongo_client
-
 __all__ = [
     "mongo_database_context"
 ]
 
-mongo_database_context: Callable[[], MongoClient] = lambda: mongo_client
+mongo_database_context: Callable[[], MongoClient] = lambda: MongoClient('localhost', 27017)
