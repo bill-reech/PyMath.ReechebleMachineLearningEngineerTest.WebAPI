39af8c375af8b41cffffffa3d58d2dfed9a17b3f
MliloM <mduduzi.mlilo@hotmail.com>
2024-03-21 08:21:27 +0200

    chore: Update response model and refactor AddLoanApplicationCommandHandler
    Changed the response model from UUID to AddLoanApplicationResponseDTO in '/loan_application' route. Refactored AddLoanApplicationCommandHandler to use AbstractLoanRepository instead of LoanRepository. Also added request_amount field to LoanRequest model and updated the method to return loan_amount and loan_granted fields.

--- reecheble_finance/application/services/loan_service/add_loan/add_loan_application_handler.py
+++ reecheble_finance/application/services/loan_service/add_loan/add_loan_application_handler.py
@@ -6,7 +6,6 @@ from typing import Type
 
 from reecheble_finance.domain.models.loan_request import LoanRequest
 from reecheble_finance.application.services.abstract_service import AbstractApplicationService
-from reecheble_finance.infrastructure.data_access.repositories.loan_repository.loan_repository import LoanRepository
 from reecheble_finance.infrastructure.data_access.repositories.loan_repository.abstract_loan_repository import (
     AbstractLoanRepository)
 from reecheble_finance.application.services.loan_service.add_loan.add_loan_application_command import (
@@ -17,9 +16,9 @@ from reecheble_finance.application.sdk.dtos.add_loan_application.add_loan_applic
 
 class AddLoanApplicationCommandHandler(AbstractApplicationService):
 
-    def __init__(self, context, repository: Type[LoanRepository]) -> None:
+    def __init__(self, context, repository: Type[AbstractLoanRepository]) -> None:
         super().__init__(context=context)
-        self.repository: LoanRepository = repository(context=self.context)
+        self.repository: AbstractLoanRepository = repository(context=self.context)
 
     async def handle(self, command: AddLoanApplicationCommand) -> AddLoanApplicationResponseDTO:
         """
@@ -31,7 +30,8 @@ class AddLoanApplicationCommandHandler(AbstractApplicationService):
         loan_request = LoanRequest(
             account=command.details.account,
             interest_rate=command.details.interest_rate,
-            payment_period_in_months=command.details.payment_period_in_months
+            payment_period_in_months=command.details.payment_period_in_months,
+            request_amount=command.details.request_amount
         )
         loan = self.repository.add(request=loan_request)
-        return AddLoanApplicationResponseDTO(id=loan.id)
+        return AddLoanApplicationResponseDTO(id=loan.id, loan_amount=loan_request.request_amount, loan_granted=True)
