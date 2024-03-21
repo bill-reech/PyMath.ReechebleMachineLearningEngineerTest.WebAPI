842f3cedb4292c6b08a40dac39eb4a38ca1991db
MliloM <mduduzimlilo@hotmail.com>
2024-03-21 10:05:04 +0200

    chore: Remove SqlAlchemyRelationalAbstractRepository class from abstract_repository
    The SqlAlchemyRelationalAbstractRepository class was removed from the abstract_repository file. This change maintains the repository hierarchy by only including the AbstractRepository in this file. The method sync(), which was specific only to SqlAlchemy, is also removed, simplifying the repository abstraction.
    
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

--- reecheble_finance/domain/abstract_repository/abstract_repository.py
+++ reecheble_finance/domain/abstract_repository/abstract_repository.py
@@ -2,16 +2,15 @@
 Abstract repositories.
 """
 
-from typing import TypeVar
 from abc import ABC, abstractmethod
+from typing import TypeVar
 
 from reecheble_finance.domain.abstract_domain.abstract_domain_parser_mixin import AbstractDomainParser
 
 __all__ = [
     "ApplicationModel",
     "RepositoryContextSession",
-    "AbstractRepository",
-    "SqlAlchemyRelationalAbstractRepository"
+    "AbstractRepository"
 ]
 
 
@@ -44,16 +43,3 @@ class AbstractRepository(ABC):
 
     @abstractmethod
     def delete(self, id_): ...
-
-
-class SqlAlchemyRelationalAbstractRepository(AbstractRepository, ABC):
-    """
-    Abstract repository using the SqlAlchemy as the `Object Relational Mapper`.
-    """
-
-    def __init__(self, context: RepositoryContextSession) -> None:
-        self.relational_context: RepositoryContextSession = context
-        super().__init__(context=self.relational_context)
-
-    def sync(self) -> None:
-        self.context.flush()
