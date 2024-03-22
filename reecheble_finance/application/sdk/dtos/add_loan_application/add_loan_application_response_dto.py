813b0f9b2d5205537d8c33e1cd05f50a6263780f
MliloM <mduduzimlilo@hotmail.com>
2024-03-22 22:40:18 +0200

    chore: Add loan installment details to loan application response
    This inclusion delivers a more comprehensive loan data in the response after adding a new loan. It includes values such as outstanding balance, the amount of interest paid, principal paid, and the total amount paid towards the loan.
    
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

--- reecheble_finance/application/sdk/dtos/add_loan_application/add_loan_application_response_dto.py
+++ reecheble_finance/application/sdk/dtos/add_loan_application/add_loan_application_response_dto.py
@@ -14,3 +14,7 @@ class AddLoanApplicationResponseDTO(BaseDomainParserMixin):
     loan_amount: confloat(ge=0)
     loan_granted: bool
     equated_monthly_installment: confloat(ge=0)
+    loan_outstanding_balance: confloat(ge=0)
+    interest_paid: confloat(ge=0)
+    principal_paid: confloat(ge=0)
+    amount_paid: confloat(ge=0)
