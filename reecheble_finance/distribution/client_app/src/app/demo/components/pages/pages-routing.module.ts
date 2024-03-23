b6b4274edf83f24a4c933e12e36283e226744d9d
MliloM <mduduzi.mlilo@hotmail.com>
2024-03-23 11:55:51 +0200

    chore: Add initial Angular frontend template.
    Signed-off-by: MliloM <mduduzimlilo@hotmail.com>

---
+++ reecheble_finance/distribution/client_app/src/app/demo/components/pages/pages-routing.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+    imports: [RouterModule.forChild([
+        {path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule)},
+        {
+            path: 'feature-management',
+            loadChildren: () => import('./feature-management/feature-management.module').then(m => m.FeatureManagementModule)
+        },
+        {path: 'empty', loadChildren: () => import('./empty/emptydemo.module').then(m => m.EmptyDemoModule)},
+        {
+            path: 'timeline',
+            loadChildren: () => import('./timeline/timelinedemo.module').then(m => m.TimelineDemoModule)
+        },
+        {path: '**', redirectTo: '/notfound'}
+    ])],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule {
+}
